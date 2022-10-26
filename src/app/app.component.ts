edb862f579bf5499a71ff6ad02b334d0772bb62d
Hagay Haut <hagayhaut@gmail.com>
2022-10-25 21:21:34 -0400

    learn state, structural & attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'teamapp';
+  newMemberName = '';
+  members: string[] = [];
+
+  onInput(member: string) {
+    this.newMemberName = member;
+  }
+
+  addMember() {
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
+  }
 }
