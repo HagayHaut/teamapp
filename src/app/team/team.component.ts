790d9e995a5c550cb74dc34f0e49928ee04ba8dd
Hagay Haut <hagayhaut@gmail.com>
2022-10-28 15:52:26 -0400

    refactor team into component using "props"

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.css']
+  styleUrls: ['./team.component.css'],
 })
 export class TeamComponent implements OnInit {
+  // like React props
+  @Input() team: string[] = [];
+  @Input() index: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
