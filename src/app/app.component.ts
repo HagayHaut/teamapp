592f550e6bbab774b100f6f824126dc9bb05b757
Hagay Haut <hagayhaut@gmail.com>
2022-10-28 15:19:44 -0400

    add conditional rendering and logic operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TitleStrategy } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +7,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'teamapp';
+  errorMessage = '';
+
   newMemberName = '';
   members: string[] = [];
 
-  onInput(member: string) {
+  numTeams: number | '' = '';
+  teams: string[][] = [];
+
+  onNameInput(member: string) {
     this.newMemberName = member;
   }
 
+  onNumTeamsInput(value: string) {
+    this.numTeams = parseInt(value);
+  }
+
   addMember() {
+    if (!this.newMemberName) {
+      this.errorMessage = "Name can't ne empty";
+      return;
+    }
+    this.errorMessage = '';
     this.members.push(this.newMemberName);
     this.newMemberName = '';
+  }
+
+  generateTeams() {
+    if (!this.numTeams || this.numTeams < 0) {
+      this.errorMessage = "Number of teams can't be empty";
+      return;
+    }
+    if (this.numTeams > this.members.length) {
+      this.errorMessage =
+        "Number of teams can't be greater than the number of members";
+      return;
+    }
+
+    this.errorMessage = '';
+    const membersCopy = [...this.members];
+    const teamsConstructor: string[][] = Array(this.numTeams)
+      .fill([])
+      .map(() => []);
+
+    let i = 0;
+    while (membersCopy.length) {
+      const randomIdx = Math.floor(Math.random() * membersCopy.length);
+      const randomMember = membersCopy[randomIdx];
+      membersCopy.splice(randomIdx, 1);
+      teamsConstructor[i % this.numTeams].push(randomMember);
+      i++;
+    }
+    this.members = [];
+    this.teams = teamsConstructor;
+    console.log(this.teams);
   }
 }
